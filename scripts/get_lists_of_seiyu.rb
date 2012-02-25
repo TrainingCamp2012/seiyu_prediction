eb3c492074e21272cd7ccd415a9a94209c1d267e
y_benjo <oreno_bukiwo_sitteruka@yahoo.co.jp>
2012-02-25 23:25:30 +0900

    不完全ながらも女性声優の共演関係グラフ

--- scripts/get_lists_of_seiyu.rb
+++ scripts/get_lists_of_seiyu.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # crawl SEIYU lists from wikipedia
 require "nokogiri"
-require "open-uri/cached"
+require "open-uri"
 require "kconv"
 
 class Crawler
@@ -33,32 +33,37 @@ end
 
 class Array
   # 声優個人板のスレタイに含まれているもののみ出力する
+
   def filtering_2ch
-    url = "http://ikura.2ch.net/voiceactor/subject.txt"
+    current  = File.dirname(File.expand_path(__FILE__))
+    url = "#{current}/../data/subject.txt"
 
     filterd_lists = Array.new
     
     self.each do |name|
-        open(url,"r:cp932").toutf8.each_line do |title|
-            if title.toutf8.include?(name)
-                filterd_lists.push name
-                next
-            end
+      open(url){|f|
+        f.each do |title|
+          if title.toutf8.include?(name)
+            filterd_lists.push name
+            next
+          end
         end
+      }
     end
     filterd_lists
   end
-  
 end
 
 if $0 == __FILE__
   d = Crawler.new
   # 男性声優の場合は
-  url = "/w/index.php?title=Category:%E6%97%A5%E6%9C%AC%E3%81%AE%E7%94%B7%E6%80%A7%E5%A3%B0%E5%84%AA"
+  # url = "/w/index.php?title=Category:%E6%97%A5%E6%9C%AC%E3%81%AE%E7%94%B7%E6%80%A7%E5%A3%B0%E5%84%AA"
   
   # 女性声優のみ  
-  # url = "/w/index.php?title=Category:%E6%97%A5%E6%9C%AC%E3%81%AE%E5%A5%B3%E6%80%A7%E5%A3%B0%E5%84%AA"
+  url = "/w/index.php?title=Category:%E6%97%A5%E6%9C%AC%E3%81%AE%E5%A5%B3%E6%80%A7%E5%A3%B0%E5%84%AA"
   d.get_list(url)
+  d.seiyus.each{|name| puts name}
+  puts "-"*20
   seiyus = d.seiyus.filtering_2ch
   seiyus.each{|name| puts name}
 end
